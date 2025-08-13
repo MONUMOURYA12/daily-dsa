db4d22272c0988eea596d3dce2d75faad251adca
Monu Mourya <128247908+MONUMOURYA12@users.noreply.github.com>
2025-08-13 04:45:27 +0000

    Day 1: Added linear search program

---
+++ linearsearch.java
@@ -0,0 +1,26 @@
+import  java.util.*;
+
+class array{
+//    secondary fuction to excute the code\
+public static int linearsearch(int num[],int key) {
+    for (int i = 0; i<num.length; i++) {
+        if(num[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//    main fuction of array
+    public static void main(String[] args) {
+        int num[]={1,2,3,4,5,6,7,8,9,10};
+        int key=5;
+        int index=linearsearch(num,key);
+        if (index == -1) {
+            System.out.println("not found");
+        }
+        else {
+            System.out.println("key is at index:" + index);
+        }
+    }
+}
