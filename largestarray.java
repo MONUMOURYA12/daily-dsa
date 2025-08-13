87f586ab9027643518056d105e9f3198b745dac5
Monu Mourya <128247908+MONUMOURYA12@users.noreply.github.com>
2025-08-13 05:56:09 +0000

    Organized Day 1 folder and added Largest Array program

--- largestarray.java
+++ largestarray.java
@@ -1,1 +1,22 @@
+import  java.util.*;
 
+class array{
+//    secondary fuction to excute the code\
+public static int linearsearch(int num[]) {
+    int largest= Integer.MIN_VALUE;
+    for (int i = 0; i<num.length; i++) {
+        if(largest<num[i]) {
+            largest=num[i];
+        }
+    }
+    return largest;
+}
+
+//    main fuction of array
+    public static void main(String[] args) {
+        int num[]={1,2,3,4,5,6,7,8,9,10};
+//        int key=5;
+        System.out.println("largest array:" + linearsearch(num));
+
+    }
+}
